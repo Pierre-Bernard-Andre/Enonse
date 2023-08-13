ad6740d466bcb038a988e318f5cab0e9cf2f5394
Pierre-Bernard-Andre <79697058+Pierre-Bernard-Andre@users.noreply.github.com>
2023-08-12 20:08:00 -0400

    Add files via upload

---
+++ batle.py
@@ -0,0 +1,16 @@
+
+text = input("antre yon text : ")
+
+
+compteur_a = 0
+compteur_i = 0
+
+
+for let n text:
+    if let == 'a':
+        compteur_a += 1
+    elif let == 'i':
+        compteur_i += 1
+
+print("nonb let miniskil en a:", compteur_a)
+print("nonb let miniskil en i:", compteur_i)
